c2f8d97cdd1cd616a1242e1b0bd85b57d46dfb20
Geoff <angryparsley@gmail.com>
2010-02-21 23:43:47 -0800

    Get graphs working.

--- cogtrack/test_react_go_no_go/views.py
+++ cogtrack/test_react_go_no_go/views.py
@@ -6,10 +6,32 @@ from cogtrack.cogtest.models import CogTest
 from cogtrack.test_react_go_no_go.models import GoNoGo
 
 def index(request):
-  results_list = GoNoGo.objects.all().order_by('-reaction_time')[:10]
+  chart_urls = []
+  results_list = GoNoGo.objects.all().order_by('end')[:10]
+
+  chart_url = "http://chart.apis.google.com/chart?chtt=Reaction Time&cht=lc&chxt=x,y&chs=500x250&chd=t:"
+  max_reaction_time = 0
+  for result in results_list:
+    max_reaction_time = max(max_reaction_time, result.reaction_time)
+    chart_url += "%u," % result.reaction_time
+
+  chart_url = chart_url[:-1]
+  chart_url += "&chds=0,%u" % max_reaction_time
+  chart_urls.append(chart_url)
+
+  chart_url = "http://chart.apis.google.com/chart?chtt=Error Rate&cht=lc&chxt=x,y&chs=500x250&chds=0,20&chd=t:"
+  for result in results_list:
+    chart_url += '%f,' % (float(result.errors) / result.tests)
+
+  chart_url = chart_url[:-1]
+  chart_url += "&chds=0,1"
+  chart_urls.append(chart_url)
+
+
   template = loader.get_template('tests/go_no_go/index.html')
   context = RequestContext(request, {
       'results_list': results_list,
+      'chart_urls': chart_urls,
   })
   return HttpResponse(template.render(context))
 
